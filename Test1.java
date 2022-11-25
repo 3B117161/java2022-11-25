77082596453d0c064cf27d55ff7c4c08cf00a4a1
3B117161 <114204288+3B117161@users.noreply.github.com>
2022-11-25 10:28:04 +0800

    Add files via upload
    菱形

---
+++ Test1.java
@@ -0,0 +1,18 @@
+public class Test1{
+	public static void (String[]args){
+		for(int i=0; i<=4; i++){ 
+			for(int k=0; k<=4; k++){ 
+				if (???)
+					system.out.print("*");
+				else
+					system.out.print(" ")
+			}
+			system.out.print("");
+			
+			if (???)
+				xxx
+			else	
+				000
+		}
+	}
+}
