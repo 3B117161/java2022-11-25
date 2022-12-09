37a6ddf701db5b64c0e48f1667395e8deaf63696
3B117161 <114204288+3B117161@users.noreply.github.com>
2022-12-09 11:55:26 +0800

    Add files via upload
    1209

---
+++ User.java
@@ -0,0 +1,23 @@
+public class User{
+	public static void (string[] args){
+		int[] iArr ={7,3,4,9,6};
+		
+		int temp
+		for(int x=0; x<iArr.length; x++){
+			for(int y=0; y<arr.length;y++)
+				if (arr[y]>arr[y+1]){
+					temp = arr[x];
+					arr[x] = arr[y];
+					arr[y] = temp;
+			}
+		}
+	}
+			
+		for (int x=0; x<iArr.length; x++)
+			system.out.println(">>"+iArr[x]);
+		}
+	}
+	public static void add(int x){
+		system.out.println("===>"+X);
+	}
+}
